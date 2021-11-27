4ad903258dcd9b1dce8b4d722d1648e6af640e29
Jon McManus <due@wxwhatever.com>
2021-11-28 09:12:58 +1000

    Initial commit.

---
+++ src/main/java/noobanidus/mods/toofast/TooFast.java
@@ -0,0 +1,11 @@
+package noobanidus.mods.toofast;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("toofast")
+public class TooFast {
+  public TooFast() {
+  }
+}
