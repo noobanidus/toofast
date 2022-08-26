0c0e5438ef1c6be6b40c722f9e7f72a54093df5e
CDAGaming <cstack2011@yahoo.com>
2022-08-25 22:22:15 -0500

    Architectury go brrr

---
+++ forge/src/main/java/net/zestyblaze/toofast/forge/TooFast.java
@@ -0,0 +1,11 @@
+package net.zestyblaze.toofast.forge;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("toofast")
+public class TooFast {
+  public TooFast() {
+  }
+}
